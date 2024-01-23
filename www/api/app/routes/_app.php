c356fccdc09a312cf76fecb9dd1bc3c0da99b5f8
Pedro Seoane <info@pedroseoaneprado.es>
2024-01-23 19:43:13 +0100

    Estructura creada por módulos (idea)

--- www/api/app/routes/_app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-app()->get('/', function () {
-    response()->json(['message' => 'Congrats!! You\'re on Leaf API']);
-});
-
-app()->get('/yeison', function () {
-    response()->json(['message' => 'Yeison']);
-});

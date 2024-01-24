d073652c30e6024d12f8a95e7324d8931dc009c1
HellLonely <helllonely404@gmail.com>
2024-01-24 19:13:56 +0100

    test

--- front/src/App.vue
+++ front/src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Mi prima usa javascripto" />
+      <HelloWorld msg="Mi prima usa javascripto y la tuya php" />
     </div>
   </header>
 

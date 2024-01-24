6cbaae9ac96eee289db7cc4e048dc7ad918f9730
peseoane <info@pedroseoaneprado.es>
2024-01-24 13:17:23 +0100

    Solucionado CORS <-> pruebas con certificado Lets encrypt

--- front/vite.config.js
+++ front/vite.config.js
@@ -8,6 +8,10 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: 2004
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

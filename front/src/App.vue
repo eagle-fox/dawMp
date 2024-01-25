38ec06fe3d318255b62e73e750346365607a27c4
peseoane <info@pedroseoaneprado.es>
2024-01-25 10:51:47 +0100

    Probando nuevo repositorio

--- front/src/App.vue
+++ front/src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="TESTE Mi prima usa javascripto y la tuya php" />
+      <HelloWorld msg="Mi prima usa javascripto y la tuya php" />
     </div>
   </header>
 

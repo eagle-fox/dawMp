c105cbedfab81864bf38e0b47bf555f601ff3b98
HellLonely <helllonely404@gmail.com>
2024-01-25 14:03:13 +0100

    Install and Test Bootstrp

--- front/src/main.js
+++ front/src/main.js
@@ -2,4 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
+// Estilos globales para Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 createApp(App).use(router).mount('#app');
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -4,6 +4,7 @@
 
 <template>
   <div>Router Add</div>
+  <button class="btn btn-primary">test</button>
 </template>
 
 <style scoped>

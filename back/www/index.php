65331c6ab55a0725ed3a544070513fc72a8b1c5c
peseoane <info@pedroseoaneprado.es>
2024-01-25 14:24:10 +0100

    Soporte de docker-compose.yml y composer php

--- back/www/index.php
+++ back/www/index.php
@@ -1,3 +1,13 @@
 <?php
-echo "Hola Iago";
+// Importa el paquete Leaf
+require 'vendor/autoload.php';
+
+$app = new Leaf\App;
+
+$app->get('/demo', function() {
+    echo "hola mundo";
+});
+
+$app->run();
+
 echo phpinfo();

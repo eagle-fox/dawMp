e83c5861f91b75274c7931dbb4717f9ac6ec97d2
Pedro Seoane <info@pedroseoaneprado.es>
2024-01-28 12:51:10 +0100

    SQL progress, working on #15

--- db/init.sql
+++ db/init.sql
@@ -3,40 +3,50 @@
  TODO: Create schema.
  */
 
-CREATE TABLE `rol`
-(
-    `id`   int          NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`),
-) ENGINE = InnoDB
+CREATE DATABASE IF NOT EXISTS `eagle-fox` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
-INSERT INTO `rol` (`name`) VALUES
-('ADMIN'),
-('USER'),
-('GUEST'),
-('IoT');
 
-CREATE TABLE `user`
+CREATE TABLE IF NOT EXISTS `user`
 (
     `id`               int          NOT NULL AUTO_INCREMENT,
     `nombre`           varchar(255) NOT NULL,
-    `nombre_segundo`   varchar(255)          DEFAULT NULL,
+    `nombre_segundo`   varchar(255) DEFAULT NULL,
     `apellido_primero` varchar(255) NOT NULL,
     `apellido_segundo` varchar(255) NOT NULL,
     `email`            varchar(255) NOT NULL UNIQUE,
     `password`         varchar(255) NOT NULL,
-    `rol`              int          NOT NULL DEFAULT 3,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`rol`) REFERENCES `rol` (`id`)
-) ENGINE = InnoDB
+    `rol`              int          NOT NULL DEFAULT 3 REFERENCES `rol` (`id`),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
-CREATE TABLE `log`
+CREATE TABLE IF NOT EXISTS `rol`
 (
-    `id`     int           NOT NULL AUTO_INCREMENT,
-    `user`   int           NOT NULL,
-    `date`   datetime      NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `ip`     varchar(255)  NOT NULL, /* IPv4 or IPv6 support? we could use a char but premature optimization... TODO */
-    `action` varchar(1024) NOT NULL,
+    `id`   int          NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+INSERT INTO `rol` (`name`) VALUES
+                               ('ADMIN'),
+                               ('USER'),
+                               ('GUEST'),
+                               ('IoT') ON DUPLICATE KEY UPDATE `name` = VALUES(`name`);
+
+CREATE TABLE IF NOT EXISTS `user_rol`
+(
+    `id`   int NOT NULL AUTO_INCREMENT,
+    `user` int NOT NULL,
+    `rol`  int NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`user`) REFERENCES `user` (`id`)
-) ENGINE = InnoDB
+    FOREIGN KEY (`user`) REFERENCES `user` (`id`),
+    FOREIGN KEY (`rol`) REFERENCES `rol` (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `log`
+(
+    `id`      int          NOT NULL AUTO_INCREMENT,
+    `user`    int          NOT NULL REFERENCES `user` (`id`),
+    `message` varchar(255) NOT NULL,
+    `date`    datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;

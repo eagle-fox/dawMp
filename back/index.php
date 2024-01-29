fdf0ba86ca100334b2dbecaa6fd87b29e664a214
Pedro Seoane <info@pedroseoaneprado.es>
2024-01-29 17:51:49 +0100

    Redirección de / a api/

--- back/index.php
+++ back/index.php
@@ -1,9 +1,2 @@
 <?php
-
-require __DIR__ . '/vendor/autoload.php';
-
-app()->get('/', function() {
-    response()->json(['message' => 'Hola Mundo']);
-});
-
-app()->run();
+header('Location: ./api/');

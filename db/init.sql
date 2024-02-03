4792da44ff4291080c59fed596a716faa9bf798b
Pedro Seoane <info@pedroseoaneprado.es>
2024-02-03 17:26:24 +0100

    Optimización: R-TREE para posición geoespacial vez de B-TREE. #15

--- db/init.sql
+++ db/init.sql
@@ -10,31 +10,21 @@ CREATE TABLE IF NOT EXISTS `rol`
 
 CREATE TABLE IF NOT EXISTS `user`
 (
-    `id`               int          NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `nombre`           varchar(255) NOT NULL,
-    `nombre_segundo`   varchar(255)          DEFAULT NULL,
-    `apellido_primero` varchar(255) NOT NULL,
-    `apellido_segundo` varchar(255) NOT NULL,
-    `email`            varchar(255) NOT NULL UNIQUE,
-    `password`         varchar(255) NOT NULL,
-    `rol`              int          NOT NULL DEFAULT 3,
-    FOREIGN KEY (`rol`) REFERENCES `rol` (`id`)
-) ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS `user_rol`
-(
-    `id`   int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user` int NOT NULL,
-    `rol`  int NOT NULL,
-    FOREIGN KEY (`user`) REFERENCES `user` (`id`),
-    FOREIGN KEY (`rol`) REFERENCES `rol` (`id`)
+    `id`               int                                   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `nombre`           varchar(255)                          NOT NULL,
+    `nombre_segundo`   varchar(255)                                   DEFAULT NULL,
+    `apellido_primero` varchar(255)                          NOT NULL,
+    `apellido_segundo` varchar(255)                          NOT NULL,
+    `email`            varchar(255)                          NOT NULL UNIQUE,
+    `password`         varchar(255)                          NOT NULL,
+    `rol`              enum ('ADMIN', 'USER', 'GUEST','IOT') NOT NULL DEFAULT 'GUEST'
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `log`
 (
     `id`      int          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `user`    int          NOT NULL,
-    `message` varchar(255) NOT NULL,
+    `message` varchar(255) NOT NULL COMMENT 'Principalmente para control de acceso (no necesariamente los de Apache)',
     `date`    datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (`user`) REFERENCES `user` (`id`)
 ) ENGINE = InnoDB;
@@ -42,32 +32,27 @@ CREATE TABLE IF NOT EXISTS `log`
 CREATE TABLE IF NOT EXISTS `iot_devices`
 (
     `id`    int      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `uuid`  CHAR(36) NOT NULL UNIQUE,
-    `token` CHAR(36) NOT NULL UNIQUE
+    `uuid`  CHAR(36) NOT NULL UNIQUE COMMENT '128 bits UUID (RFC 4122)',
+    `token` CHAR(64) NOT NULL UNIQUE COMMENT '256 bits token',
+    INDEX `idx_uuid` (`uuid`) USING HASH COMMENT 'Sólo soporta igualdad'
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `iot_devices_user`
 (
-    `id`      int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `user`    int NOT NULL,
-    `device`  int NOT NULL,
-    `date`    datetime     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `id`     int      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `user`   int      NOT NULL,
+    `device` int      NOT NULL,
+    `date`   datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (`user`) REFERENCES `user` (`id`),
     FOREIGN KEY (`device`) REFERENCES `iot_devices` (`id`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `iot_data`
 (
-    `id`      int      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `device`  int      NOT NULL,
-    `date`    datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `latitud` double   NOT NULL,
-    `longitud` double  NOT NULL,
+    `id`       int      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `device`   int      NOT NULL,
+    `date`     datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `location` POINT    NOT NULL,
+    SPATIAL INDEX `idx_location` (`location`),
     FOREIGN KEY (`device`) REFERENCES `iot_devices` (`id`)
 ) ENGINE = InnoDB;
-
-INSERT IGNORE INTO `rol` (`name`)
-VALUES ('ADMIN'),
-       ('USER'),
-       ('GUEST'),
-       ('IoT');

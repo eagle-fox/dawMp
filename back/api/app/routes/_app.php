ed606f9b1296164a7a1d7e03ef63f2289cc35681
Pedro Seoane <info@pedroseoaneprado.es>
2024-02-03 17:54:51 +0100

    #28 pruebas con la Api iniciales

--- back/api/app/routes/_app.php
+++ back/api/app/routes/_app.php
@@ -1,6 +1,13 @@
 <?php
-
+declare(strict_types=1);
 
 app()->get('/', function () {
     response()->json(['message' => 'Congrats!! You\'re on Leaf API']);
+});
+
+app()->get('/v0/dumpTables', function () {
+    db()->autoConnect();
+    $tables = db()->query("SHOW TABLES");
+    $tables = $tables->fetchAll();
+    response()->json($tables);
 });

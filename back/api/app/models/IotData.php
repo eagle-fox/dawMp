6eac260725e441d091108447e321580d21733e2a
Pedro Seoane <info@pedroseoaneprado.es>
2024-02-03 21:15:04 +0100

    #28 Añadidos modelos E/R a ORM.

---
+++ back/api/app/models/IotData.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Leaf\Model;
+
+class IotData extends Model
+{
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'device' => null,
+        'location' => '',
+    ];
+
+    /**
+     * Formato compatible con DateTime de MySQL.
+     *
+     * @var string
+     */
+    protected $dateFormat = 'Y-m-d H:i:s';
+
+    /**
+     * Exactamente el nombre de la tabla que se va a utilizar desde el DDL.
+     *
+     * @var string
+     */
+    protected $table = 'iot_data';
+
+    /**
+     * La clave primaria, por defecto, es 'id'.
+     *
+     * @var int
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = true;
+
+    /**
+     * Los atributos accesibles para el modelo en masa.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'device', 'location',
+    ];
+
+    /**
+     * Todas va con sellado de tiempo.
+     *
+     * @var bool
+     */
+    public $timestamps = true;
+
+    /**
+     * Get the iot device that owns the data.
+     */
+    public function device()
+    {
+        return $thi>s->belongsTo(IotDevice::class);
+    }
+}

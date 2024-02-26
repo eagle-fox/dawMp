8dfb6ce30c77c6a26d7c2b703de3c22755d33898
peseoane <info@pedroseoaneprado.es>
2024-02-26 14:11:30 +0100

    #26 Búsqueda de posiciones por cercanía.

--- back/api/app/controllers/Utils.php
+++ back/api/app/controllers/Utils.php
@@ -2,15 +2,13 @@
 
 namespace app\controllers;
 
-use app\models\client;
-use app\models\user;
-use Illuminate\Database\Eloquent\Collection;
-use Random\RandomException;
-
 class Utils
 {
-    static function calculateNewCoordinates(float $latitude,float $longitude,float $distance): array {
-        $latMid = $latitude; // as we are calculating for this point, we can use the latitude directly
+    static function calculateNewCoordinates(float $latitude, float $longitude, float $distance): array
+    {
+        // Álgebra lineal...
+
+        $latMid = $latitude;
 
         $m_per_deg_lat = 111132.954 - 559.822 * cos(2.0 * deg2rad($latMid)) + 1.175 * cos(4.0 * deg2rad($latMid));
         $m_per_deg_lon = 111132.954 * cos(deg2rad($latMid));
@@ -24,6 +22,10 @@ class Utils
         $max_longitude = $longitude + $deltaLon;
         $min_longitude = $longitude - $deltaLon;
 
-        return ['max_latitude' => $max_latitude, 'min_latitude' => $min_latitude, 'max_longitude' => $max_longitude, 'min_longitude' => $min_longitude];
+        return ['max_latitude'  => $max_latitude,
+                'min_latitude'  => $min_latitude,
+                'max_longitude' => $max_longitude,
+                'min_longitude' => $min_longitude
+        ];
     }
 }

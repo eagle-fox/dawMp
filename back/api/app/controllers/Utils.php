ae3ffed42522e1d9dcf8526373c919769671693d
peseoane <info@pedroseoaneprado.es>
2024-02-26 14:13:15 +0100

    #26 Documentación.

--- back/api/app/controllers/Utils.php
+++ back/api/app/controllers/Utils.php
@@ -4,6 +4,20 @@ namespace app\controllers;
 
 class Utils
 {
+    /**
+     * Calcula las nuevas coordenadas basándose en una latitud y longitud dadas, y una distancia.
+     *
+     * Este método utiliza álgebra lineal para calcular las nuevas coordenadas. Primero, calcula
+     * los metros por grado de latitud y longitud en la latitud dada. Luego, calcula el cambio en
+     * latitud y longitud dividiendo la distancia por los metros por grado de latitud y longitud,
+     * respectivamente. Finalmente, calcula las nuevas latitudes y longitudes sumando y restando
+     * el cambio en latitud y longitud a la latitud y longitud dadas, respectivamente.
+     *
+     * @param float $latitude La latitud actual.
+     * @param float $longitude La longitud actual.
+     * @param float $distance La distancia a añadir o restar a la latitud y longitud actuales.
+     * @return array Un array asociativo con las nuevas latitudes y longitudes máximas y mínimas.
+     */
     static function calculateNewCoordinates(float $latitude, float $longitude, float $distance): array
     {
         // Álgebra lineal...
@@ -22,10 +36,11 @@ class Utils
         $max_longitude = $longitude + $deltaLon;
         $min_longitude = $longitude - $deltaLon;
 
-        return ['max_latitude'  => $max_latitude,
-                'min_latitude'  => $min_latitude,
-                'max_longitude' => $max_longitude,
-                'min_longitude' => $min_longitude
+        return [
+            'max_latitude'  => $max_latitude,
+            'min_latitude'  => $min_latitude,
+            'max_longitude' => $max_longitude,
+            'min_longitude' => $min_longitude
         ];
     }
 }

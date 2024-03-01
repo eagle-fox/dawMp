2853aaf2392e95c77305fd65b8b6859c35a2d080
peseoane <info@pedroseoaneprado.es>
2024-03-01 13:38:53 +0100

    #28 mejoras en generacion aleatoria de fechas de ncimiento

--- back/api/app/routes/_app.php
+++ back/api/app/routes/_app.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+use app\models\IotDevice;
+
 require_once __DIR__ . "/user.php";
 require_once __DIR__ . "/iotDevices.php";
 require_once __DIR__ . "/iotData.php";
@@ -11,3 +13,14 @@ app()->get("/", function () {
 });
 
 app()->get("/demo","DemoController@create");
+app()->get("/cumpleaños", function () {
+        // get all IoT Devices
+        $devices = IotDevice::query()->get();
+        foreach ($devices as $device) {
+            // set a random creation date
+            $device->created_at = date("Y-m-d H:i:s", rand(0, time()));
+            $device->save();
+            error_log("Device updated: " . json_encode($device));
+        }
+        response()->json(["message" => "Cumpleaños actualizados"]);
+});

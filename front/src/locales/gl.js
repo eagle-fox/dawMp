7889fd43631ef8e24b6b646960a6b8e531edbea0
HellLonely <helllonely404@gmail.com>
2024-06-15 17:03:41 +0200

    Update Languages

--- front/src/locales/gl.js
+++ front/src/locales/gl.js
@@ -1,57 +1,60 @@
 let miscelaneusTextPackage = {
   welcome: 'Benvido',
   home: 'Inicio',
-  logout: 'Cerrar sesion',
+  logout: 'Pechar sesión',
+  dashboard: 'Panel de Animais',
   loading: 'Cargando',
-  mounths: 'Meses',
-  years: 'Anos',
+  mounths: 'Mes',
+  days: 'Día',
+  years: 'Ano',
   date: 'Data',
   age: 'Idade',
   name_page: 'Eagle Fox',
   pets: 'Mascotas',
+  birthday: 'Aniversario'
 }
 
 let langTextPackage = {
   lang_es: 'Español',
   lang_sh: 'Inglés',
   lang_de: 'Alemán',
-  lang_gl: 'Gallego',
+  lang_gl: 'Galego',
 }
 
 let homeTextPackage = {
-  home_t1: 'Comeza a localizar ao teu animal de compañía agora mesmo!',
-  home_t2: 'En calquera lugar, a calquera hora. Mantén seguro ao teu animal de compañía.',
+  home_t1: 'Comeza a localizar á túa mascota agora mesmo!',
+  home_t2: 'En calquera lugar, a calquera hora. Mantén segura á túa mascota.',
   home_t3:
-      'Proporcionamos aos nosos usuarios un mapa interactivo para ver en todo momento ao seu animal de compañía. Conta cunha gran precisión e alto detalle.',
+      'Proporcionamos aos nosos usuarios un mapa interactivo para ver en todo momento á súa mascota. Conta cunha gran precisión e alto detalle.',
   home_t4: 'Que ofrecemos aos nosos usuarios:',
   home_t5: 'Rexistra o teu colar!!',
   home_t6:
       'Dentro da caixa do teu colar Eagle Fox haberá unha tarxeta co código do colar, deberás introducir ese código na sección de Rexistro Mascota',
-  home_t7: 'Introduce o código !!',
-  home_t8: 'Co código proporcionado poderás rexistrar ao teu animal de compañía',
-  home_t9: 'Listo, xa podes ver ao teu animal de compañía !!',
+  home_t7: 'Introduce o código!!',
+  home_t8: 'Co código proporcionado poderás rexistrar á túa mascota',
+  home_t9: 'Listo, xa podes ver á túa mascota!!',
   home_t10:
-      'Agora xa poderás usar todas as características de monitorización do teu pequeno peludo',
-  home_t11: '¿Como configurar o teu colar Eagle Fox?',
+      'Agora xa poderás usar todas as características de monitorización da túa pequena bola de pelo',
+  home_t11: 'Como configurar o teu colar Eagle Fox?',
   home_t12: 'Monitorización en tempo real.',
-  home_bd1: 'Posicionamento dos teus animais de compañía nun mapa en tempo real',
+  home_bd1: 'Posicionamento das túas mascotas nun mapa en tempo real',
   home_bd2: 'Poñer puntos de interese e establecer rutas personalizadas',
   home_bd3:
-      'Obter información sobre o teu animal de compañía, como tempo de paseo, Kcal gastadas ...',
+      'Obter información sobre a túa mascota, como tempo de paseo, Kcal gastadas...',
   home_js1: 'Introduce o código do colar',
   home_js2: 'Rexistrar Mascota 🐱',
   home_bt1: 'Comezar',
 }
 
 let animalRegisterForm = {
-  arf_t1: 'Rexistra ao teu animal de compañía',
-  arf_t2: 'Nome do teu animal de compañía',
+  arf_t1: 'Rexistra á túa mascota',
+  arf_t2: 'Nome da túa mascota',
   arf_t3: 'Raza',
   arf_js1: 'Código non válido',
-  arf_b1: 'Cán',
+  arf_b1: 'Can',
   arf_b2: 'Gato',
-  arf_b3: 'Coneixo',
-  arf_b4: 'Hamster',
+  arf_b3: 'Coello',
+  arf_b4: 'Hámster',
 }
 
 let loginTextPackage = {
@@ -62,9 +65,10 @@ let loginTextPackage = {
   login_secondSubname: 'Segundo Apelido',
   login_email: 'Correo electrónico',
   login_password: 'Contrasinal',
-  login_rpassword: 'Repete o contrasinal',
+  login_rpassword: 'Repita o contrasinal',
   login_message: 'Nunca compartiremos o teu correo electrónico con ninguén.',
   login_message1: 'Crear conta',
+  login_error: 'Email ou contrasinal incorrectos'
 }
 
 let aboutTextPackage = {
@@ -72,8 +76,8 @@ let aboutTextPackage = {
 }
 
 let footer = {
-  footer_direction: 'Dirección',
-  footer_number: 'Teléfono',
+footer_direction: 'Enderezo',
+footer_number: 'Teléfono',
   footer_mail: 'Correo electrónico',
 }
 
@@ -85,8 +89,17 @@ let animaloptions = {
   anios: "anos",
   distancia: "Distancia",
   eliminar: "Eliminar",
-  editar: "Editar"
-};  
+  name: "Nome",
+  specie: "Especie",
+  dog: "Can",
+  pig: "Porco",
+  cat: "Gato",
+  cow: "Vaca",
+  sheep: "Oveja",
+  edit: "Editar",
+  save: "Gardar",
+  cancel: "Cancelar"
+};
 
 export default {
   home: homeTextPackage,
